f2255fcc3b751c674d5b487fee921924a0b8f32e
Andreas Stalder <astalder@hsr.ch>
2016-11-22 23:07:27 +0100

    Evaluator & Validator

--- prog/src/service/Evaluator.py
+++ prog/src/service/Evaluator.py
@@ -3,3 +3,12 @@ class Evaluator:
     def __init__(self, testSuite):
         self.testSuite = testSuite
 
+    def compare(self, testCase):
+        if(testCase.actualResult == testCase.expectedResult):
+            print('\033[92m' + testCase.name + ": Test bestanden -------------------------" + '\033[0m')
+        else:
+            print('\033[91m' + testCase.name + ": Test nicht bestanden -------------------" + '\033[0m')
+
+    def compareAll(self):
+        for test in self.testSuite.testCases:
+            self.compare(test)
--- prog/src/service/FileValidator.py
+++ prog/src/service/FileValidator.py
@@ -1,1 +1,18 @@
-#Todo
+import subprocess
+import os
+import sys
+
+class FileValidator:
+
+
+    def __init__(self, testFile, deviceFile):
+        self.testFile = testFile
+        self.deviceFile = deviceFile
+
+
+    def validate(self):
+        FNULL = open(os.devnull, 'w')
+        procTest = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/testSchema.yaml' ], stdout=subprocess.PIPE, stderr=FNULL, shell=True)
+        procDev = subprocess.Popen(['pykwalify -d ' + self.deviceFile + ' -s ' + os.getcwd() + '/src/service/deviceSchema.yaml' ], stdout=subprocess.PIPE,  stderr=FNULL, shell=True)
+        print("Test-File: \n" + procTest.communicate()[0].decode('utf-8'))
+        print("Device-File: \n" + procDev.communicate()[0].decode('utf-8'))
