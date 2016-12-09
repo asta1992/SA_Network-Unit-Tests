b50340b5a52270b7f7d93ba517cac89e27043248
Andreas Stalder <astalder@hsr.ch>
2016-12-09 14:00:38 +0100

    Refactor nuts argparse

--- prog/nuts.py
+++ prog/nuts.py
@@ -1,35 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import os
-import getopt
+import argparse
 from src.application.ValidationController import ValidationController
 from src.application.TestController import TestController
 
 
 
 def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "hv:i:", ["help", "validate=", "input="])
-    except getopt.GetoptError:
-        printUsage()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            printUsage()
-            sys.exit()
-        elif opt in ("-v", "--validate"):
-            validator = ValidationController(os.getcwd() + "/" + arg, os.getcwd() + "/" + args[0])
-            validator.logic()
-        elif opt in ("-i", "--input"):
-            tester = TestController(os.getcwd() + "/" + arg, os.getcwd() + "/" + args[0])
-            tester.logic()
-            #argparse
-            #doc string
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Start with a Testfile and a Devicefile", nargs=2)
+    parser.add_argument("-v", "--validate", help="Creates local account", nargs=2, )
+    parser.add_argument("-vt", "--validatetest", help="Creates local account", nargs=1,)
+    parser.add_argument("-vd", "--validatedevice", help="Creates local account", nargs=1, )
 
-
-def printUsage():
-    print("usage")
-
+    args = parser.parse_args()
+    if args.input:
+        tester = TestController(os.getcwd() + "/" + args.input[0], os.getcwd() + "/" + args.input[1])
+        tester.logic()
+    elif args.validate:
+        validator = ValidationController(os.getcwd() + "/" + args.validate[0], os.getcwd() + "/" + args.validate[1])
+        validator.logic()
+    elif args.validatetest:
+        validator = ValidationController(os.getcwd() + "/" + args.validatetest[0], "T")
+        validator.logic()
+    elif args.validatedevice:
+        validator = ValidationController(os.getcwd() + "/" + args.validatedevice[0], "D")
+        validator.logic()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- prog/src/service/FileValidator.py
+++ prog/src/service/FileValidator.py
@@ -12,7 +12,16 @@ class FileValidator:
 
     def validate(self):
         FNULL = open(os.devnull, 'w')
-        procTest = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/testSchema.yaml' ], stdout=subprocess.PIPE, stderr=FNULL, shell=True)
-        procDev = subprocess.Popen(['pykwalify -d ' + self.deviceFile + ' -s ' + os.getcwd() + '/src/service/deviceSchema.yaml' ], stdout=subprocess.PIPE,  stderr=FNULL, shell=True)
-        print("Test-File: \n" + procTest.communicate()[0].decode('utf-8'))
-        print("Device-File: \n" + procDev.communicate()[0].decode('utf-8'))
+        if self.deviceFile == "T":
+            procTest = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/testSchema.yaml'],stdout=subprocess.PIPE, stderr=FNULL, shell=True)
+            print("Test-File: \n" + procTest.communicate()[0].decode('utf-8'))
+        elif self.deviceFile == "D":
+            procDev = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/deviceSchema.yaml' ], stdout=subprocess.PIPE,  stderr=FNULL, shell=True)
+            print("Device-File: \n" + procDev.communicate()[0].decode('utf-8'))
+        else:
+            procTest = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/testSchema.yaml'],stdout=subprocess.PIPE, stderr=FNULL, shell=True)
+            procDev = subprocess.Popen(['pykwalify -d ' + self.deviceFile + ' -s ' + os.getcwd() + '/src/service/deviceSchema.yaml'],stdout=subprocess.PIPE, stderr=FNULL, shell=True)
+            print("Test-File: \n" + procTest.communicate()[0].decode('utf-8'))
+            print("Device-File: \n" + procDev.communicate()[0].decode('utf-8'))
+
+
